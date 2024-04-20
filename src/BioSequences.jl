0e9e09b8d5bb29452ab4006a1a82e2b5dcca33e6
Marco Antoniotti <marco.antoniotti@unimib.it>
2024-04-20 18:24:05 +0200

    Fixed dependency on JLCL.

--- src/BioSequences.jl
+++ src/BioSequences.jl
@@ -8,9 +8,9 @@
 ### This file is a part of BioJulia.
 ### License is MIT: https://github.com/BioJulia/BioSequences.jl/blob/master/LICENSE
 
-using JLCL
-
 module BioSequences
+
+using JLCL
 
 export
     ###
